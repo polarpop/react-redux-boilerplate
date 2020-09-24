0554950ffc693e6aa74c3540ab066e7fba8424e7
Douglas Schamberg <doug.schamberg@gmail.com>
2020-09-24 04:59:27 -0700

    first commit

--- src/reducers/routes.ts
+++ src/reducers/routes.ts
@@ -13,26 +13,30 @@ export interface RouteAction {
   payload: AppRoute;
 }
 
-const defaultState: AppRoute[] = [{
-  id: 1,
-  component: () => import('../components/AppLoading'),
-  props: {
-    path: '/',
-    exact: false
-  },
-  secure: false
-}];
+/**
+ * @example
+ * const defaultState = [{
+ * id: 1,
+ * component: () => import('../components/AppLoading'),
+ * props: {
+ *   path: '/',
+ *   exact: false
+ * },
+ * secure: false
+ * }]
+*/
+const defaultState: AppRoute[] = [];
 
 export function routes(state: AppRoute[] = defaultState, action: RouteAction) {
   switch (action.type) {
     case constants.routes.ROUTE_ADDED:
       const newRoutes = [...state];
-      if (!state.find((route: any) => route.id === action.payload.id)) {
+      if (!state.find((route: AppRoute) => route.id === action.payload.id)) {
         newRoutes.push(action.payload);
       }
       return newRoutes;
     case constants.routes.ROUTE_REPLACED:
-      return state.filter((route: any) => route.id !== action.payload.id);
+      return state.filter((route: AppRoute) => route.id !== action.payload.id);
     default:
       return state;
   }
